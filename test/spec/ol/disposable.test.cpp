cb820432b609559b2810f2b5da100c6f2c0c1ec4
Yury Kuznetsov <yury.kuznetsov@pds.nl>
2018-07-13 15:04:54 -0500

    Added disposable test.

--- test/spec/ol/disposable.test.cpp
+++ test/spec/ol/disposable.test.cpp
@@ -1,44 +1,56 @@
-import Disposable from '../../../src/ol/Disposable.js';
+#include <gtest/gtest.h>
 
+#include "../../../src/ol/Disposable.h"
 
-describe('ol.Disposable', function() {
+using namespace ol;
 
-  describe('constructor', function() {
-
-    it('creates an instance', function() {
-      const disposable = new Disposable();
-      expect(disposable).to.be.a(Disposable);
-    });
-
-  });
-
-  describe('#disposed_', function() {
-
-    it('is initially false', function() {
-      const disposable = new Disposable();
-      expect(disposable.disposed_).to.be(false);
-    });
-
-    it('is true after a call to dispose', function() {
-      const disposable = new Disposable();
-      disposable.dispose();
-      expect(disposable.disposed_).to.be(true);
-    });
-
-  });
-
-  describe('#dispose()', function() {
-
-    it('calls disposeInternal only once', function() {
-      const disposable = new Disposable();
-      sinon.spy(disposable, 'disposeInternal');
-      expect(disposable.disposeInternal.called).to.be(false);
-      disposable.dispose();
-      expect(disposable.disposeInternal.callCount).to.be(1);
-      disposable.dispose();
-      expect(disposable.disposeInternal.callCount).to.be(1);
-    });
-
-  });
-
-});
+TEST(ol_Disposable, t1)
+{
+    //
+    //  describe('constructor', function() {
+    //
+    //    it('creates an instance', function() {
+    {
+        auto disposable = Disposable();
+        //      expect(disposable).to.be.a(Disposable);
+    }
+    //    });
+    //
+    //  });
+    //
+    //  describe('#disposed_', function() {
+    //
+    //    it('is initially false', function() {
+    {
+        //      const disposable = new Disposable();
+        //      expect(disposable.disposed_).to.be(false);
+    }
+    //    });
+    //
+    //    it('is true after a call to dispose', function() {
+    {
+        //      const disposable = new Disposable();
+        //      disposable.dispose();
+        //      expect(disposable.disposed_).to.be(true);
+    }
+    //    });
+    //
+    //  });
+    //
+    //  describe('#dispose()', function() {
+    //
+    //    it('calls disposeInternal only once', function() {
+    {
+        //      const disposable = new Disposable();
+        //      sinon.spy(disposable, 'disposeInternal');
+        //      expect(disposable.disposeInternal.called).to.be(false);
+        //      disposable.dispose();
+        //      expect(disposable.disposeInternal.callCount).to.be(1);
+        //      disposable.dispose();
+        //      expect(disposable.disposeInternal.callCount).to.be(1);
+    }
+    //    });
+    //
+    //  });
+    //
+}
